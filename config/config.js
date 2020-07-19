2c6a2be6e1ed507a76c78745436c2b7d4b791c7c
jhonedisonrod21 <jhonedisonrod21@gmail.com>
2020-07-19 14:35:12 -0500

    solucion error de coneccion a la bd 2

--- config/config.js
+++ config/config.js
@@ -7,7 +7,6 @@ const port = process.env.PORT || 3000;
 */
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev'; ///esto define si estamos en desarrollo local o ya en produccion
 
-
 const mongoUSR = 'coffeadmin';
 const mongoPSW = 'djjLEh2RUHKRQv4w';
 const mongoDB  = 'cafe'
