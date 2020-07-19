da3f154123afd38556433dc0e43593239adac197
jhonedisonrod21 <jhonedisonrod21@gmail.com>
2020-07-19 14:27:24 -0500

    solucion error

--- config/config.js
+++ config/config.js
@@ -10,7 +10,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev'; ///esto define si estamos 
 const mongoUSR = 'coffeadmin';
 const mongoPSW = 'djjLEh2RUHKRQv4w';
 const mongoDB  = 'cafe'
-const mongoURI = '';
+var mongoURI = '';
 if(process.env.NODE_ENV === 'dev'){
     mongoURI = 'mongodb://jhon:jerm1998@127.0.0.1:27017/cafe?connectTimeoutMS=1000&bufferCommands=false'
 }else{
