372bfeee6ea62d23eb8e45f119911f07a6e129d1
jhonedisonrod21 <jhonedisonrod21@gmail.com>
2020-07-19 15:18:32 -0500

    variable deentorno mongo

--- config/config.js
+++ config/config.js
@@ -6,15 +6,12 @@ const port = process.env.PORT || 3000;
 *      Database params
 */
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev'; ///esto define si estamos en desarrollo local o ya en produccion
-
-const mongoUSR = 'coffeadmin';
-const mongoPSW = 'djjLEh2RUHKRQv4w';
-const mongoDB  = 'cafe'
 var mongoURI = '';
+
 if(process.env.NODE_ENV === 'dev'){
     mongoURI = 'mongodb://jhon:jerm1998@127.0.0.1:27017/cafe?connectTimeoutMS=1000&bufferCommands=false'
 }else{
-    mongoURI = `mongodb+srv://${mongoUSR}:${mongoPSW}@mynodecluster.8hrsl.mongodb.net/${mongoDB}?retryWrites=true&w=majority`; // base de datos en la nube con mongo atlas
+    mongoURI = process.env.MONGO_URI; // base de datos en la nube con mongo atlas
 }
 
 /*
